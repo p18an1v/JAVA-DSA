e0f255089de7d638618f702a437f081c710b22c2
p18an1v <pranavmhargude@gmail.com>
2024-09-19 11:46:01 +0530

    add quesiton in this program

--- InterviewQuestion/findMissingNumber.java
+++ InterviewQuestion/findMissingNumber.java
@@ -1,5 +1,9 @@
 package InterviewQuestion;
-
+ // Given an array nums of n integers where nums[i] is in the range [1, n], 
+//return an array of  all the integers in the range [1, n] that do not appear in nums.
+// Example 1:
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Expected Output: [5,6]
 public class findMissingNumber {
 
     public static void missNum(int nums[]){
@@ -17,7 +21,9 @@ public class findMissingNumber {
         for(int i =0; i<nums.length;i++){
             System.out.print(nums[i]+" ");
         }
+
         System.out.println();
+
         //check
         for(int i = 0; i<nums.length;i++){
             if(nums[i]!=i+1){
